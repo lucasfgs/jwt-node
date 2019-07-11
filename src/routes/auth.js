3fa5aadbeb8b91edd287bb5f91840cefd4edca00
Lucas Ferreira <blooodcf@gmail.com>
2019-07-11 11:04:11 -0300

    Password encrypted when new user is created

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,16 +1,36 @@
 const app = require("express").Router();
 const User = require("../model/User");
+const bcrypt = require("bcrypt");
 const { registerValidaion } = require("../validations/user");
 
-app.get("/", (req, res) => {
-  res.send("OK");
+app.get("/", async (req, res) => {
+  try {
+    let result = await User.find();
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.get("/:id", async (req, res) => {
+  let id = req.params.id;
+
+  try {
+    let result = await User.findOne({ _id: id });
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 app.post("/register", async (req, res) => {
+  // Cryptography password
+  let hashedPassowd = bcrypt.hashSync(req.body.password, 10);
+
   let user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: hashedPassowd
   });
 
   // Validations
@@ -27,6 +47,13 @@ app.post("/register", async (req, res) => {
   } catch (error) {
     res.status(400).send(error);
   }
+});
+
+app.delete("/:id", (req, res) => {
+  User.deleteOne({ _id: req.params.id }, err => {
+    if (err) res.status(400).send(error);
+    res.status(200).send("User removed successfully");
+  });
 });
 
 module.exports = app;
